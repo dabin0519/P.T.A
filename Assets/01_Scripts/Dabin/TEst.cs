09d6eff9016f33842fe7ad30fd9af12591f81102
dabin0519 <dadabin0519@naver.com>
2023-05-31 18:10:31 +0900

    ..

---
+++ Assets/01_Scripts/Dabin/TEst.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TEst : MonoBehaviour
+{
+    [SerializeField] private float a; // Inspector
+
+    private void Update()
+    {
+    }
+}
