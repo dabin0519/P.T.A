dee86517c2e67bfc258c6bd76c007806411d504d
dabin0519 <dadabin0519@naver.com>
2023-06-23 01:05:45 +0900

    dialogue(~ing)

--- Assets/01_Scripts/Dabin/Dialogue.cs
+++ Assets/01_Scripts/Dabin/Dialogue.cs
@@ -9,12 +9,16 @@ public class Dialogue : MonoBehaviour
 {
     [SerializeField] private TMP_Text _nickName;
     [SerializeField] private TMP_Text _contents;
-    [SerializeField] private GameObject _dialoguePanel;
 
+    private Image _panel;
     private AudioSource _audio;
+    private DialogueSO _dialogueSO;
+
+    private bool _isFade;
 
     private void Start() 
     {
+        _panel = GetComponent<Image>();
         _audio = _contents.GetComponent<AudioSource>();
         _contents.maxVisibleCharacters = 0;
         DOTween.To(x => _contents.maxVisibleCharacters = (int)x, 0f, _contents.text.Length, 1f).SetEase(Ease.Linear);
@@ -22,7 +26,35 @@ public class Dialogue : MonoBehaviour
 
     public void ShowText(DialogueSO dialogueSO)
     {
+        _dialogueSO = dialogueSO;
+        DisplayDialogue(true);
         StartCoroutine(WriteContents(dialogueSO));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopCoroutine(WriteContents(_dialogueSO));
+            _contents.maxVisibleCharacters = _dialogueSO.Contents.Length;
+            StartCoroutine(ClickSpace());
+        }
+    }
+
+    private IEnumerator ClickSpace()
+    {
+        if (_dialogueSO.IsEnd)
+        {
+            _isFade = false;
+            DisplayDialogue(false);
+
+            yield return new WaitForSeconds(1.2f);
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            DialogueManger.Instance.IsEnd = true;
+        }
     }
 
     private IEnumerator WriteContents(DialogueSO dialogueSO)
@@ -35,24 +67,24 @@ public class Dialogue : MonoBehaviour
         yield return new WaitForSeconds(1f);
         _audio.Stop();
 
-        if (dialogueSO.IsEnd)
-        {
-            DisplayDialogue(false);
-        }
+        DialogueManger.Instance.IsEnd = true;
     }
+
 
     public void DisplayDialogue(bool value)
     {
-        Image panel = gameObject.GetComponent<Image>();
-        gameObject.SetActive(value);
-
         if (value) //켜질때
         {
-            panel.DOFade(255, 1f);
+            gameObject.SetActive(true);
+            _panel.DOFade(0.7f, 1f);
+            _nickName.DOFade(0.7f, 1f);
+            _contents.DOFade(0.7f, 1f);
         }
         else //꺼질때
         {
-            panel.DOFade(0, 1f);
+            _panel.DOFade(0, 1f);
+            _nickName.DOFade(0, 1f);
+            _contents.DOFade(0, 1f);
         }
     }
 }
